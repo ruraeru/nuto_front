bfbb794f086be73aa077a9617dcef49b55d2f871
ruraeru <f93335383@gmail.com>
2025-07-02 21:07:34 +0900

    design: /dashboard/spendingHistory 페이지 디자인 구현

--- src/app/(tabs)/dashboard/spendingHistory/page.tsx
+++ src/app/(tabs)/dashboard/spendingHistory/page.tsx
@@ -1,3 +1,5 @@
+import { FunnelIcon, MagnifyingGlassIcon } from "@heroicons/react/24/solid"
+
 export default function spendingHistory() {
     return (
         <div className="">
@@ -7,36 +9,109 @@ export default function spendingHistory() {
             </div>
             <div className="">
                 <div className="w-full h-36">
-                    <div>
-                        <ul className="flex gap-5 *:p-3 *:rounded-xl px-5 *:cursor-pointer *:hover:bg-neutral-500 *:hover:text-white bg-neutral-200  rounded-xl w-fit">
-                            <li>날짜</li>
-                            <li>카테고리</li>
-                            <li>금액</li>
+                    <div className="flex flex-col gap-6">
+                        <div className="flex gap-6">
+                            <ul className="flex items-center gap-5 px-5 font-medium border-1 border-[#D1D1D1] rounded-xl w-fit h-12 
+                        shadow-lg
+                        *:h-9 *:border-1 *:border-[#C1E7F0] *:hover:bg-[#C1E7F0]
+                        *:cursor-pointer
+                        *:p-3 *:rounded-xl
+                        *:flex *:place-items-center
+                        ">
+                                <li>All</li>
+                                <li>Month</li>
+                                <li>Category</li>
+                                <li>Card</li>
+                            </ul>
+
+                            {/* search input */}
+                            <div className="relative flex items-center w-[606px] h-[50px] rounded-xl border border-[#D1D1D1] shadow-xl">
+                                <input type="text" className="
+                                flex-auto h-full rounded-xl px-5 bg-transparent outline-none" placeholder="검색어 입력" />
+
+                                <MagnifyingGlassIcon width={19} className="absolute right-4 cursor-pointer" />
+                            </div>
+                        </div>
+
+                        <ul className="flex gap-4 *:flex *:gap-1 *:border-2 *:border-[#7CBBDE] *:px-5 *:py-2  *:rounded-3xl *:font-medium">
+                            <li>
+                                <FunnelIcon fill="none" stroke="black" width={14} />
+                                의류/화장품
+                            </li>
+                            <li>
+                                <FunnelIcon fill="none" stroke="black" width={14} />
+                                교통비
+                            </li>
+                            <li>
+                                <FunnelIcon fill="none" stroke="black" width={14} />
+                                식비
+                            </li>
                         </ul>
                     </div>
                 </div>
-                <table className="w-full">
-                    <thead className="border-b-1">
-                        <tr className="*:py-2 text-left">
-                            <th>이름</th>
-                            <th>금액</th>
-                            <th>카드</th>
-                            <th>날짜</th>
-                            <th>카테고리</th>
-                        </tr>
-                    </thead>
-                    <tbody className="font-bold">
-                        {[1, 2, 3, 4, 5].map((_, index) => (
-                            <tr key={index} className="*:py-5 border-b-1">
-                                <td>버거킹</td>
-                                <td>8,400</td>
-                                <td>신한카드</td>
-                                <td>2025-01-23</td>
-                                <td>음식</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+
+                <div className="w-[1006px]">
+                    <div className="h-[500px] border-2 border-[#C1E7F0] rounded-2xl p-3">
+                        <table className="w-full">
+                            <thead className="border-b-2 border-[#C1E7F0]">
+                                <tr className="*:p-5">
+                                    <th>이름</th>
+                                    <th>금액</th>
+                                    <th>카드</th>
+                                    <th>날짜</th>
+                                    <th>카테고리</th>
+                                </tr>
+                            </thead>
+                            <tbody className="text-center font-medium *:border-b-1 *:border-[#C1E7F0] *:last:border-none ">
+                                <tr className="*:px-5 *:py-7">
+                                    <td>
+                                        버거킹
+                                    </td>
+                                    <td>10,000</td>
+                                    <td>신한은행</td>
+                                    <td>25.04.11</td>
+                                    <td>식비</td>
+                                </tr>
+                                <tr className="*:px-5 *:py-7">
+                                    <td>
+                                        버거킹
+                                    </td>
+                                    <td>10,000</td>
+                                    <td>신한은행</td>
+                                    <td>25.04.11</td>
+                                    <td>식비</td>
+                                </tr>
+                                <tr className="*:px-5 *:py-7">
+                                    <td>
+                                        버거킹
+                                    </td>
+                                    <td>10,000</td>
+                                    <td>신한은행</td>
+                                    <td>25.04.11</td>
+                                    <td>식비</td>
+                                </tr>
+                                <tr className="*:px-5 *:py-7">
+                                    <td>
+                                        버거킹
+                                    </td>
+                                    <td>10,000</td>
+                                    <td>신한은행</td>
+                                    <td>25.04.11</td>
+                                    <td>식비</td>
+                                </tr>
+                                <tr className="*:px-5 *:py-7">
+                                    <td>
+                                        버거킹
+                                    </td>
+                                    <td>10,000</td>
+                                    <td>신한은행</td>
+                                    <td>25.04.11</td>
+                                    <td>식비</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     )
