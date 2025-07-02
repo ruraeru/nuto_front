ce6bd0b2e9352bceb7cb45fdc85c6535f84c0c3b
ruraeru <f93335383@gmail.com>
2025-07-02 20:41:22 +0900

    design: /dashboard/cards/graph 디자인 적용

--- src/app/(tabs)/dashboard/cards/graph/page.tsx
+++ src/app/(tabs)/dashboard/cards/graph/page.tsx
@@ -27,15 +27,15 @@ ChartJS.register(
 export default function CardsGraph() {
     return (
         <div>
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-4 mb-4">
                 <h1 className="text-5xl font-bold">카드 총액</h1>
                 <p className="text-lg/tight font-semibold">
                     매일매일 얼마나 소비했는지 궁금하셨나요? 한 달 동안의 소비 변화를 직관적으로 확인할 수 있습니다. <br />
-                    가장 많이 소비한 날과 가장 적게 소비한 날을 바로 파악할 수 있으며,<br />그래프를 클릭하면 해당 날의 상세 소비 내역도 확인할 수 있습니다.
+                    가장 많이 소비한 날과 가장 적게 소비한 날을 바로 파악할 수 있습니다.
                 </p>
             </div>
             <div className="flex flex-col items-center gap-4">
-                <div className="h-[460px] w-full">
+                <div className="w-[1006px] h-[460px] p-5 border-2 border-[#C1E7F0] rounded-lg">
                     <Line
                         options={{
                             scales: {
@@ -44,7 +44,12 @@ export default function CardsGraph() {
                                     title: {
                                         display: false,
                                         text: '일',
+
                                     },
+                                    grid: {
+                                        drawOnChartArea: false,
+                                        drawTicks: false,
+                                    }
                                 },
                                 y: {
                                     title: {
@@ -52,8 +57,15 @@ export default function CardsGraph() {
                                         text: '값',
                                     },
                                     ticks: {
-                                        stepSize: 10, // y축 간격 설정
+                                        stepSize: 50, // y축 간격 설정
                                     },
+                                    border: {
+                                        display: false,
+                                    },
+                                    grid: {
+                                        color: "#C1E7F0"
+                                    },
+                                    min: 1
                                 },
                             },
                             plugins: {
@@ -74,37 +86,31 @@ export default function CardsGraph() {
                                 {
                                     label: '데이터',
                                     data: [
-                                        1, 50, 5, 17, 150, 25, 200,
+                                        1, 50, 100, 17, 150, 25, 200,
                                     ],
-                                    borderColor: 'black',
-                                    borderWidth: 2,
+                                    borderColor: '#7CBBDE',
+                                    borderWidth: 3,
                                     fill: false,
-                                    tension: 0.3, // 선을 부드럽게 만드는 옵션
+                                    tension: 0.4, // 선을 부드럽게 만드는 옵션
                                     pointRadius: 0, // 데이터 포인트 원 숨김
+                                    cubicInterpolationMode: "monotone"
                                 },
                             ]
                         }}
                     />
                 </div>
-                <div className="*:bg-gray-400 *:px-3 *:rounded-lg flex gap-4">
+                <div className="*:border-1 *:px-3 *:py-1 *:rounded-lg flex gap-4">
                     <select>
                         <option>2024</option>
                         <option>2025</option>
                         <option>2026</option>
                     </select>
                     <select>
-                        <option>1월</option>
-                        <option>2월</option>
                         <option>3월</option>
                         <option>4월</option>
                         <option>5월</option>
                         <option>6월</option>
                         <option>7월</option>
-                        <option>8월</option>
-                        <option>9월</option>
-                        <option>10월</option>
-                        <option>11월</option>
-                        <option>12월</option>
                     </select>
                 </div>
             </div>
