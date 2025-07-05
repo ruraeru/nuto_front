bee68be494a48c3100f1f6c31aecdcccb0f62290
ruraeru <f93335383@gmail.com>
2025-07-05 19:26:33 +0900

    feat: 로그인 성공하면 백엔드로부터 accessToken, refreshToken을 받아 세션 쿠키에 저장함

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -51,3 +51,70 @@ export async function signUp(formData: SignUpProps) {
 
   return res;
 }
+
+export async function AuthLogin({
+  userId,
+  password,
+}: {
+  userId: string;
+  password: string;
+}) {
+  try {
+    const response = await fetch(`${API_URL}/api/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId,
+        password,
+      }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.log(errorData);
+      console.error("AuthLogin API 에러: ", errorData);
+
+      return {
+        success: false,
+        message:
+          errorData.message ||
+          "로그인에 실패했습니다. 아이디 또는 비밀번호를 확인해주세요.",
+        statusCode: response.status,
+      };
+    }
+
+    const data = await response.json();
+
+    return {
+      success: true,
+      message: "로그인 성공!",
+      accessToken: data.data.accessToken,
+      refreshToken: data.data.refreshToken,
+      userId,
+    };
+  } catch (err) {
+    console.error("AuthLogin 네트워크 오류 : ", err);
+    return {
+      success: false,
+      message: "서버에 연결할 수 없습니다. 네트워크 상태를 확인해주세요.",
+      error: err instanceof Error ? err.message : String(err),
+    };
+  }
+}
+
+export async function verificationToken(accessToken: string) {
+  const response = await fetch(`${API_URL}/api/auth/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + accessToken,
+    },
+  });
+  if (response.ok) {
+    const data = await response.text();
+    console.log(data);
+    // return await response.json();
+  }
+}
