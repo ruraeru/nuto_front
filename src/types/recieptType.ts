15269fd89c469577e73ddbc330e57fb6d0cd6e69
ruraeru <f93335383@gmail.com>
2025-07-06 16:40:34 +0900

    feat: 영수증 데이터에 수입 지출 타입 추가함

--- src/types/recieptType.ts
+++ src/types/recieptType.ts
@@ -1,13 +1,17 @@
-export type RecieptType = {
+export type TransactionType = "수입" | "지출";
+
+export interface RecieptType {
+  recieptName: string;
   storeName: string;
   address: string;
   purchaseDate: string;
   category: string[];
   totalAmount: number;
-  recieptName: string;
-};
+  transactionType: TransactionType; // 수입/지출 타입 추가
+}
 
 export interface IinitialState {
   output: RecieptType | null;
   prompt: string;
+  error?: string;
 }
