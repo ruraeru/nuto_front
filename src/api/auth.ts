38abcada20cc058b15dcad7e61dff04686c4e3e4
ruraeru <f93335383@gmail.com>
2025-07-07 14:48:02 +0900

    feat: 토큰 갱신

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,3 +1,6 @@
+"use server";
+
+import getSession from "@/lib/session";
 import { AuthApiResponse, authFetch } from "./api";
 
 export async function sendEmailCode(email: string): Promise<AuthApiResponse> {
@@ -46,21 +49,34 @@ export async function AuthLogin({
   return result;
 }
 
-export async function verificationToken(
-  accessToken: string
-): Promise<AuthApiResponse<string>> {
-  if (!accessToken) {
-    return {
-      success: false,
-      message: "Access Token이 제공되지 않았습니다.",
-      statusCode: 400,
-    };
-  }
+export async function verificationToken(): Promise<AuthApiResponse<string>> {
+  const session = await getSession();
   return authFetch(
     "", // API_URL/api/auth/ 바로 뒤에 아무것도 없음
     "POST",
     undefined, // POST 요청이지만 본문 없음
-    { Authorization: "Bearer " + accessToken },
+    { Authorization: "Bearer " + session.accessToken },
     "text" // 응답을 텍스트로 파싱
   );
+}
+
+interface IreissueToken {
+  grantType: string;
+  accessToken: string;
+  refreshToken: string;
+}
+
+//토큰 재발급 함수 추후에 서버에서 만료된 토큰으로 요청을 주면 토큰이 만료된 토큰이라는 리턴을 줘야함...
+export async function reissueToken() {
+  const session = await getSession();
+  const result = await authFetch<IreissueToken>("reissue", "POST", {
+    accessToken: session.accessToken,
+    refreshToken: session.refreshToken,
+  });
+
+  if (result.success && result.data) {
+    session.accessToken = result.data.accessToken;
+    session.refreshToken = result.data.refreshToken;
+    await session.save();
+  }
 }
